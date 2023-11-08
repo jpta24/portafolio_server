1d5a3eda1aa8f40fda45368da48ef744a2c732be
Jpta24 <jeanpierretorres@gmail.com>
2023-11-08 15:01:20 +0100

    MongoDB done

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const app = require("./app");
+const app = require('./app');
 
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
+const connectDB = require('./db/index');
+
+// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5008
 const PORT = process.env.PORT || 5008;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+connectDB().then(() => {
+	app.listen(PORT, () => {
+		console.log(`Server listening on http://localhost:${PORT}`);
+	});
 });
